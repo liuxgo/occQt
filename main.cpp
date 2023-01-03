bb24b07f824067da5766f3dfb1109fa1fa54aa25
刘欣 <liu.xin@iungoprint.com>
2023-01-03 14:52:38 +0800

    feat：删除原始项目主窗口

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
-#include "mainwindow.h"
+/*
+ * @Author: 刘欣 liu.xin@iungoprint.com
+ * @Date: 2023-01-01 19:49:50
+ * @LastEditors: 刘欣 liu.xin@iungoprint.com
+ * @LastEditTime: 2023-01-01 20:03:28
+ * @Description:
+ * Copyright (c) 2023 by iUNGO, All Rights Reserved.
+ */
+/*
+ *    Copyright (c) 2018 Shing Liu All Rights Reserved.
+ *
+ *           File : main.cpp
+ *         Author : Shing Liu(eryar@163.com)
+ *           Date : 2018-01-08 20:00
+ *        Version : OpenCASCADE7.2.0 & Qt5.7.1
+ *
+ *    Description : OpenCASCADE with Qt demo.
+ */
 
+#include "occQt.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+
+    occQt w;
     w.show();
+
     return a.exec();
 }
